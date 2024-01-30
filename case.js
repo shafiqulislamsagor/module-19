ab329ac13f011ce29c8f858b20c7ed2eb510a7f5
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-31 03:45:22 +0600

    module 19-1

---
+++ case.js
@@ -0,0 +1,8 @@
+const sting = 'Shafiqul ';
+const point = ['sagor','rahim','tumi','ami','apni'];
+
+console.log(sting.length);
+console.log(point.length);
+
+console.log(sting[1]);
+console.log(point[1]);
