3b98e9d125a18b11f9b1e63acbb176a5e2879cf0
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-31 04:08:37 +0600

    module 19-2

--- case.js
+++ case.js
@@ -6,3 +6,33 @@ console.log(point.length);
 
 console.log(sting[1]);
 console.log(point[1]);
+
+const subject = 55;
+const book = '55';
+
+if(subject == book){
+    console.log('grade A+')
+}
+else{
+    console.log('passed grade D')
+}
+
+const myName = 'Shafiqul Islam Sagor';
+
+console.log(myName)
+console.log(myName.toUpperCase())
+console.log(myName.toLowerCase())
+
+
+const sub = 'hi mama';
+const bok = 'Hi mama';
+
+let subs = sub.toUpperCase()
+let boks = bok.toUpperCase()
+
+if(subs === boks){
+    console.log('valo laglo')
+}
+else{
+    console.log('hoi nai')
+}
