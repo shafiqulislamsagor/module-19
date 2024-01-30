fe8f6e71eb3467e2dae4fcb833bf87332b4f58ea
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-01-31 05:52:39 +0600

    module 19-4

--- slice.js
+++ slice.js
@@ -21,3 +21,26 @@ const fullName = st.concat(" ").concat(md).concat(' ').concat(lt) ;
 console.log(fullName);
 
 console.log(fullName.includes('I'))
+
+const sentences = 'Shafiqul Islam Sagor';
+
+let reverse = '';
+
+for(const letter of sentences){
+    reverse = letter + reverse
+}
+console.log(reverse)
+let ulta = '';
+
+for(let i = 0 ; i < sentences.length ; i++){
+    const letter = sentences[i];
+    console.log(letter);
+    ulta = letter + ulta ;
+}
+
+console.log(ulta)
+
+
+const shortUlta = sentences.split('').reverse().join('');
+
+console.log(shortUlta)
