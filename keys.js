f0774aad207e2663459cf014a3d7f2858c97ad2f
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-01 22:27:45 +0600

    something

--- keys.js
+++ keys.js
@@ -10,10 +10,24 @@ const kets  = Object.keys(computer);
 
 console.log('keys name --',kets)
 
+for(const i of kets){
+    console.log(i , ":" , computer[i])
+}
+
 const valu = Object.values(computer);
 
 console.log('values name -- ',valu);
 
+for(const x of valu){
+    console.log(x)
+}
+
 delete computer.brand;
 
+computer.newBrand = 'Oppo'
+
 console.log(computer)
+
+for(const pop in computer){
+    console.log(pop , computer[pop])
+}
---
+++ new.js
@@ -0,0 +1,30 @@
+// const per = {
+//     brand : ' econo',
+//     price : 10 , 
+//     color : 'black'
+// }
+
+// console.log(per);
+
+// const rubber = new Object({
+//     k : 'ami'
+// })
+
+// console.log(rubber)
+
+// const saban = Object.create({
+//     per : 'red',
+//     khata : 'white',
+//     'robat per' : 'ami kisu jani na'
+// })
+
+// saban.new = '100'
+
+// console.log(saban)
+
+// const newObj = Object.create( { name: { value  :"test" }, age: {value: 23} });
+// console.log(newObj.name);
+
+const saban = Object.create({names : 'test' , new : '200'})
+
+console.log(saban.new)
